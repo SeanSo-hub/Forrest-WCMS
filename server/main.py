226368f8ffc729b913e5aab11c6bd498181cef49
SeanSo-hub <pabsean@yahoo.com>
2024-05-20 19:55:45 +0800

    Project initialization

---
+++ server/main.py
@@ -0,0 +1,79 @@
+from flask import request, jsonify
+from config import app, db
+from model import User, Plan
+
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    json_users = list(map(lambda x: x.to_json(), users))
+    return jsonify({"users": json_users})
+
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    fullname = request.json.get("fullname")
+    contact_number = request.json.get("contactNumber")
+    user_type = request.json.get("userType")
+
+    if not fullname or not contact_number:
+        return ( 
+            jsonify({"message": "You must include a fullname and a contact number"}),
+            400, 
+        )
+    
+    new_user = User(
+        fullname = fullname, 
+        contact_number = contact_number, 
+        user_type = user_type
+    )
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created successfully!"}), 201
+
+
+@app.route("/update_user/<int:user_id>")
+def update_user(user_id):
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"message": "User not found!"}), 404
+    
+    data = request.json
+    user.fullname = data.get("fullname", user.fullname)
+    user.contact_number = data.get("contactNumber", user.contact_number)
+    user.user_type = data.get("userType", user.user_type)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated successfully!"}, 200)
+
+
+@app.route("/delete_user/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"message": "User not found!"}), 404
+    
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted successfully!"}, 200)
+
+
+@app.route("/plans", methods=["GET"])
+def get_plans():
+    plans = Plan.query.all()
+    json_plans = list(map(lambda x: x.to_json(), plans))
+    return jsonify({"plans": json_plans})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)    
---
+++ server/model.py
@@ -0,0 +1,41 @@
+from config import db
+from enum import Enum
+
+class UserType(Enum):
+    STUDENT = "student"
+    EMPLOYEE = "employee"
+    OTHERS = "others"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(80), unique=False, nullable=False)
+    contact_number = db.Column(db.String(11), unique=False, nullable=False)
+    user_type = db.Column(db.Enum(UserType), nullable=False)
+
+    # Foreign key column referencing the Plan model
+    plan_id = db.Column(db.Integer, db.ForeignKey('plan.id'), nullable=True)
+    
+    # Define the relationship with the Plan model
+    plan = db.relationship('Plan', backref=db.backref('users', lazy=True))
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "fullName": self.fullname,
+            "contactNumber": self.contact_number,
+            "userType": self.user_type.value,
+        }
+    
+class Plan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price
+        }
